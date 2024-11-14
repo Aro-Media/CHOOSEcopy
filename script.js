b48419048b6a3a7beba50ccbeb0e86ecbc83a593
Aro-Media <dwwallace@cps.edu>
2024-11-14 17:26:52 +0000

    third commit

--- script.js
+++ script.js
@@ -1,37 +1,57 @@
 // html button elements
 let yesButton = document.querySelector('.oneButton');
 let noButton = document.querySelector('.twoButton');
-let tryagain = document.querySelector('retrybutton');
+
+let tryagain = document.querySelector('.retry');
+tryagain.style.display = "none";
+let tryagain2 = document.querySelector('.retry2');
+tryagain2.style.display = "none";
+
+let HLine = document.querySelector('.HLine');
+HLine.style.display = "none";
+let CLine = document.querySelector('.N-HLine');
+CLine.style.display = "none";
+
+let Below = document.querySelector('.Below');
+Below.style.display = "none";
+let Across = document.querySelector('.Across');
+Across.style.display = "none";
+
+let restart = document.querySelector('.restart');
+let taxi = document.querySelector('HLine');
+let giveup = document.querySelector('.twoButton');
 
 // html content elements
-let choice1image = document.querySelector('.choice1image');
-choice1image.style.display = "none";
-
 let text = document.querySelector('.text');
 
-let choice1Aimage = document.querySelector('.choice1Aimage');
-choice1Aimage.style.display ="none";
 
 
 
 
 yesButton.addEventListener('click', function() {
 text.innerHTML = "We need to go to Evanston. What did that man say again?";
-choice1image.style.display = "none";
-choice1Aimage.style.display ="none";
-yesButton.style.display="none";
-noButton.style.display="none";
+yesButton.style.display = "none";
+noButton.style.display = "none";
+HLine.style.display = "block";
+CLine.style.display = "block";
 });
 
 noButton.addEventListener('click', function() {
-text.innerHTML = "Free will is a myth. Try again?";
-choice1image.style.display = "none";
-choice1Aimage.style.display ="none";
-yesButton.style.display="none";
-noButton.style.display="none";
+text.innerHTML = "Free will is a myth. Reload and try again.";
+yesButton.style.display = "none";
+noButton.style.display = "none";
     });
 
-choice1Aimage.addEventListener('dblclick', function(){
-    text.innerHTML = "You make it another day"
-    choice1Aimage.src = "knockedout.jpg";
+HLine.addEventListener('click', function(){
+text.innerHTML = "We made it to the station. There's a single road heading north, with a bridge going over it holding another road that moves east/west. <br> <br> So... where do we go again?";
+HLine.style.display = "none";
+CLine.style.display = "none";
+Below.style.display = "block";
+Across.style.display = "block";
+});
+
+CLine.addEventListener('click', function() {
+text.innerHTML = "So. We're lost. That <em>might</em> not've been the right direction. <br> <br> Uh... What do we do now?";
+HLine.style.display = "none";
+CLine.style.display = "none";
 });
